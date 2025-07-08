a2d2a46e50347abb60a8ce54dd62a47bbca6f7a1
MoeKaung122 <kaungkaung034592@gmail.com>
2025-07-08 07:10:19 +0630

    variable_and_numeric

---
+++ numeric/int.py
@@ -0,0 +1,3 @@
+x = 10   # inteager
+y = 102384784097187   # inteager
+z  = -28  # inteager
---
+++ variable/variable.py
@@ -0,0 +1,9 @@
+x = 1 
+y = 2 
+z = x + y 
+print(z) 
+# variable ဆို တာ computer memory ထဲမှာ data တစ်ခုကို သိမ်းဆည်းထားတဲ့ အရာတစ်ခု ဖြစ်ပါတယ်။
+# variable တွေကို သုံးပြီး data တွေကို သိမ်းဆည်းထားနိုင်ပါတယ်
+# variable တွေက ထဲ  မှာ  သိမ်းလို ရ တဲ့ data type များ 
+# int, float, string, list, tuple, dictionary, set စတဲ့ data type များကို သိမ်းဆည်းထားနိုင်ပါတယ်
+# none , numeric , list , tuple, dictionary, set, string, boolean, complex စတဲ့ data type များကို သိမ်းဆည်းထားနိုင်ပါတယ်
