6a6bdc93d8eb312880cbfc0e636b558c46852a08
MoeKaung122 <kaungkaung034592@gmail.com>
2025-07-16 21:43:54 +0630

    printing_patterning

---
+++ loop/printing_pattern.py
@@ -0,0 +1,81 @@
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * * 
+* * * * *
+
+"""
+# nested for loop
+# for i in range(5):
+#     for j in range(5):
+#         print('*' , end=' ')
+#     print()
+
+
+
+# *1 increasing traiangle pattern 
+"""
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+"""
+# for i in range(5):
+#     for j in range(i + 1 ):
+#         print('*' , end=' ')
+#     print()
+
+
+"""
+* * * * *
+* * * * 
+* * * 
+* * 
+* 
+
+"""
+# *1 ddecreasing traiangle pattern 
+# n = 5
+# for i in range(n):
+#     for i in range(i , n ):
+#         print('*' , end=' ')
+#     print()
+
+#  right traiangle
+"""
+        *
+      * *
+    * * *
+  * * * * 
+* * * * *
+
+"""
+# n = 5
+# for i in range(n):
+#     for j in range(i , n ):
+#         print(' ' , end=' ')
+#     for j in range(i + 1  ):
+#         print('*' , end=' ')
+#     print()
+
+#HILL pattern 
+"""
+         *
+       * * *
+     * * * * *
+   * * * * * * *
+ * * * * * * * * *
+
+"""
+# n = 5
+# for i in range(n):
+#     for j in range(i , n ):
+#         print(' ' , end=' ')
+#     for j in range(i   ):
+#         print('*' , end=' ')
+#     for j in range(i + 1  ):
+#         print('*' , end=' ')
+#     print()
