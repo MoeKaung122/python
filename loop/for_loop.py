32b9110d7343d9ba8337fbd854ddf2ff77c0fdab
MoeKaung122 <kaungkaung034592@gmail.com>
2025-07-16 18:17:57 +0630

    for_and_while_loop

---
+++ loop/for_loop.py
@@ -0,0 +1,59 @@
+#  for loop
+
+# for i in range(5): 
+#     print("how are you ")
+
+# for number in range(3):
+#     print("attemp " , number , (number + 1) * ".")
+
+# series data (range , list , tuple , set , dictionary , string)
+
+# string
+# for character in "apple" :
+#     print(character)
+
+# list
+# for fruit in ['apple','orange','mango']:
+#     print(fruit)
+
+# dictionary 
+dictio = {
+    'name' : 'moe kaung',
+    'age' : 18,
+    'name2' : 'kaung kaung'
+}
+
+# for di in  dictio :
+#     print(di)
+
+# for di in  dictio.values() :
+#     print(di)
+
+# break
+# successful = True
+
+# for number in range(3) :
+#     print('attempt')
+#     if successful :
+#         print("successful")
+#         break
+# number_last = [] 
+# print(number_last) 
+# for i in range(1 , 10) :
+#     if i % 2 == 0 :
+#         number_last.append(i)
+# print(number_last)    
+
+# continue
+# successful = True
+# for number in range(3) :
+#     print('attempt')
+#     if successful :
+#         print("successful")
+#         continue
+
+# nested for loop
+
+# for i in range(3):
+#     for y in range(3):
+#         print( i , y)
---
+++ loop/while_loop.py
@@ -0,0 +1,31 @@
+# count = 0 
+# while count < 3 :
+#     print('hello word')
+#     count += 1
+
+
+# if break
+# count = 0 
+# while count < 6 :
+    
+#     count += 1
+#     if count == 4 :
+#         print(f'hello word {count}')
+#         break
+
+# if continue
+# count = 0 
+# while count < 6 :
+#     print(f'hello word {count}')
+#     count += 1
+    
+#     if count == 4 :
+        
+#         print('contineue')
+#         continue
+
+# command = ''
+
+# while command != 'quit':
+#     command = input('text ender')
+#     print(command)
