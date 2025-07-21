bee0a9a48fc18d477525cf315ab13635d9c392a7
MoeKaung122 <kaungkaung034592@gmail.com>
2025-07-21 17:33:41 +0630

    function

---
+++ function/function.py
@@ -0,0 +1,41 @@
+# def hello():
+#     print('HELLO')
+
+# hello()
+
+# def hello():
+#     pass
+
+# def hello():
+#     print('hello')
+#     print('word')
+
+# hello()
+
+# def rectangle_area(lenght , width ):
+#     area = lenght * width 
+#     return area
+
+# print(rectangle_area(4,5))
+
+# def hello(name):
+#     print(f'hello {name}')
+    
+
+# hello('moekaung')
+
+# two types of functions 
+
+# 1 လုပ်ငန် ေဆာင်းရန် 
+# 2 တန်းဖို တစ်ခုခု ပြန်ပေးရန် 
+
+# def hello():
+#     print('hello')
+#     print('word')
+
+# def rectangle_area(lenght , width ):
+#     area = lenght * width 
+#     return area
+
+# area = rectangle_area(4,5)
+# print(area)
